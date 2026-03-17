85c55a1bb2ba8eec16903ae40472cbf8c92435a1
ShaniBhai786 <roshaanahmad91@gmail.com>
2026-03-17 14:18:51 +0500

    Add scroll-to-top effect in UniSoftTech component

--- client/src/pages/UniSoftTech.jsx
+++ client/src/pages/UniSoftTech.jsx
@@ -1,8 +1,11 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Tech from '../components/Tech'
 import Instititute_Navbar from '../components/Instititute_Navbar'
 
 function UniSoftTech() {
+    useEffect(() => {
+    window.scrollTo(0, 0);
+    })
   return (
     <>
     <Instititute_Navbar /> 
