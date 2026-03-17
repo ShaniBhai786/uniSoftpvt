69b835ec8f7b0e49a8b20376eaf4f8b9e41a224d
ShaniBhai786 <roshaanahmad91@gmail.com>
2026-03-17 14:17:29 +0500

    Add scroll-to-top effect on Secondary page load

--- client/src/pages/Secondary.jsx
+++ client/src/pages/Secondary.jsx
@@ -1,8 +1,11 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import School from '../components/School'
 import Institute_Navbar from '../components/Instititute_Navbar'
 
 function Secondary() {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  })
   return (
     <div>
         <Institute_Navbar />
